ff047fc6ce334b7a0f6cff5b38a74a19893c74e7
ivy <ivy@ivy.>
2025-07-29 18:53:20 -0300

    Made it so the function returns Rock, Paper and Scissors
    Also created the letPlayerChoise function without a code in it

--- script.js
+++ script.js
@@ -1,7 +1,18 @@
 // Criar uma função que escolhe um numero de 1 a 3
 function letComputerChoice() {
 // Escolher um numero de 1 a 3
-return Math.floor(Math.random() * 3) + 1
+    let result = Math.floor(Math.random() * 3) + 1
+    if (result === 3) {
+        return "Paper"
+    } else if (result === 2) {
+        return "Rock"
+    } else {
+        return "Scissors"
+    }
 }
 
-console.log(letComputerChoice())
+
+// Criar uma função para receber a escolha do jogador
+function letPlayerChoice() {
+
+}
