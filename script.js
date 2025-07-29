9bee48b0442c2b10465b0e3315a1c2b426a1272a
ivy <ivy@ivy.>
2025-07-29 18:40:05 -0300

    Added the function letComputerChoice that selects randomly a number between 1 and 3

--- script.js
+++ script.js
@@ -1,1 +1,7 @@
-console.log("Hello world!")
+// Criar uma função que escolhe um numero de 1 a 3
+function letComputerChoice() {
+// Escolher um numero de 1 a 3
+return Math.floor(Math.random() * 3) + 1
+}
+
+console.log(letComputerChoice())
