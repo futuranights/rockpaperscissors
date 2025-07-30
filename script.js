92ce4c0923086f8e2767138e0e7b33748be08391
ivy <ivy@ivy.>
2025-07-30 18:56:56 -0300

    Finished the script for the project Added the playGame function

--- script.js
+++ script.js
@@ -19,8 +19,6 @@ function getHumanChoise() {
     return choise
 }
 // Criar duas variaveis, humanScore e computerScore, ambas iniciando no valor 0.
-var humanScore = 0
-var computerScore = 0
 
 // Fazer a logica para apenas uma partida
 function playRound(humanChoice, computerChoice) {
@@ -32,18 +30,46 @@ function playRound(humanChoice, computerChoice) {
         (computerChoice === "paper" && humanChoice === "scissors") ||
         (computerChoice === "scissors" && humanChoice === "rock") 
     ) {
-        return console.log(`You won! ${humanChoice} beats ${computerChoice}!`) || humanScore === +1
+        console.log(`You won! ${humanChoice} beats ${computerChoice}!`) 
+        return "human"
     } else if (
         (computerChoice === "rock" && humanChoice === "scissors") ||
         (computerChoice === "paper" && humanChoice === "rock") ||
         (computerChoice === "scissors" && humanChoice === "paper")
     ) {
-        return console.log(`You lose! ${computerChoice} beats ${humanChoice}!`) || computerScore === +1
+        console.log(`You lose! ${computerChoice} beats ${humanChoice}!`)
+        return "computer"
     }
 }
 
-const humanSelection = getHumanChoise()
-const computerSelection = getComputerChoice()
+function playGame() {
+    let humanScore = 0;
+    let computerScore = 0;
+    
+    for(let i=1; i <= 5; i++){
+        console.log(`--- Round ${i}!---`);
+        const humanSelection = getHumanChoise();
+        const computerSelection = getComputerChoice();
 
-playRound(humanSelection, computerSelection)
+        const result = playRound(humanSelection, computerSelection);
 
+        if (result === "human") {
+            humanScore++
+        } else if (result == "computer"){
+            computerScore++
+        }
+
+        console.log(`Score after round ${i}: You ${humanScore}, Computer ${computerScore}.`)
+    }
+
+    console.log('---Final Result---')
+    if (humanScore>computerScore){
+        console.log("You won!")
+    } else if (computerScore>humanScore) {
+        console.log("You lost!")
+    } else {
+        console.log("You tied!")
+    }
+}
+
+playGame()
